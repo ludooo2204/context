1a6279e7ec1ef1e2f5c91d62511aa9984b312661
ludovic Vachon <vachon.ludovic@gmail.com>
2021-06-16 22:46:53 +0200

    tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState,useRef,useEffect } from "react";
+import { gsap } from "gsap";
 
-function App() {
+
+
+const App = () => {
+  const [toggle,setToggle]= useState(false)
+  const [tourne,setTourne]= useState(false)
+
+  const myRef=useRef(null)
+
+  useEffect(() => {
+    tourne?
+    gsap.to(myRef.current,{
+   flex:5
+    }    )
+    :
+    gsap.to(myRef.current,{
+flex:1
+    }    )
+    
+   
+  }, [tourne])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<div style={{display:'flex'}}>
+			<h1>toto</h1>
+        <div style={toggle?div2:div1} ref={myRef}>
+          lkfzlke,vzlkevlkzev,lzve,lkzev,</div>
+        <button style={{flex:1}} onMouseMove={()=>setToggle(!toggle)}>Click moi dessus</button>
+        <button style={{flex:1}} onMouseEnter={()=>setTourne(!tourne)} onMouseLeave={()=>setTourne(!tourne)}>Click moi pour tourner</button>
+		</div>
+	);
+};
+const div1={
+  padding: '100px',
+  backgroundColor:'green'
 }
-
+const div2={
+  padding: '10px',
+  backgroundColor:'red'
+}
 export default App;
